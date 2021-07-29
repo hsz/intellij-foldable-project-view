73d013fb530e88eec521ee11e700a86c9e16957e
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2021-07-29 06:47:34 +0100

    FoldIgnoredFiles option

---
+++ src/main/kotlin/ski/chrzanow/foldableprojectview/actionSystem/FoldIgnoredFilesAction.kt
@@ -0,0 +1,30 @@
+package ski.chrzanow.foldableprojectview.actionSystem
+
+import com.intellij.ide.projectView.ProjectView
+import com.intellij.openapi.actionSystem.ToggleOptionAction
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.DumbAware
+import ski.chrzanow.foldableprojectview.settings.FoldableProjectSettings
+import java.util.function.Function
+
+class FoldIgnoredFilesAction : DumbAware, ToggleOptionAction(Function {
+    object : Option {
+
+        private val settings = it.project?.service<FoldableProjectSettings>()
+
+        override fun isSelected() = settings?.foldIgnoredFiles ?: false
+
+        override fun setSelected(selected: Boolean) {
+            val updated = selected != isSelected
+
+            settings?.foldIgnoredFiles = selected
+
+            if (updated) {
+                it.project?.let { project ->
+                    val view = ProjectView.getInstance(project)
+                    view.currentProjectViewPane?.updateFromRoot(true)
+                }
+            }
+        }
+    }
+})
