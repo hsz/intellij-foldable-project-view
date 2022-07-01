787c87cb54b5ce7862f2e14e02712679da19ca2b
Elior <heliosaian@gmail.com>
2022-07-01 16:12:05 +0300

    fix(2022): fix for 2022.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
 rootProject.name = "Foldable ProjectView"
+
+pluginManagement {
+  repositories {
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    gradlePluginPortal()
+  }
+}
